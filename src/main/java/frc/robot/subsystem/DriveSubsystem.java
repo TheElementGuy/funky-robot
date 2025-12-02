d9dc33bcd98dde5859b2e1c9abc8337eed2e6072
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2025-12-01 19:52:21 -0800

    Limiting courtesy of Jordan

--- src/main/java/frc/robot/subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystem/DriveSubsystem.java
@@ -6,12 +6,16 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveSubsystem extends SubsystemBase {
 
     private SparkMax power;
     private SparkMax steer;
+
+    private SlewRateLimiter driveLimiterr;
+    private SlewRateLimiter turnLimiter;
 
     public DriveSubsystem() {
 
@@ -21,9 +25,16 @@ public class DriveSubsystem extends SubsystemBase {
         steer = new SparkMax(6, MotorType.kBrushless);
         steer.configure(new SparkMaxConfig(), ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
+        driveLimiterr = new SlewRateLimiter(0.5);
+        turnLimiter = new SlewRateLimiter(2);
+
     }
 
     public void drive(double power, double turning) {
+        power *= 0.5;
+
+        power = driveLimiterr.calculate(power);
+        turning = turnLimiter.calculate(turning);
         this.power.set(power);
         steer.set(turning * 0.5);
     }
